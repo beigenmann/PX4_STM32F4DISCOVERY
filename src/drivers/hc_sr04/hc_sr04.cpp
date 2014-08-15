0089f751480e248d380b4988dec31ec7784de52a
Benno Eigenmann <beigenmann@gmail.com>
2014-08-15 07:47:05 +0200

    init

--- src/drivers/hc_sr04/hc_sr04.cpp
+++ src/drivers/hc_sr04/hc_sr04.cpp
@@ -4,9 +4,15 @@
  * HC-SR04 driver.
  */
 #include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <stm32.h>
 #include <nuttx/config.h>
 #include <drivers/device/device.h>
 #include <drivers/drv_hc_sr04.h>
+
+#define GPIO_TRIG (GPIO_OUTPUT|GPIO_PUSHPULL|GPIO_SPEED_2MHz|GPIO_OUTPUT_SET|GPIO_PORTC|GPIO_PIN6)
+#define GPIO_ECHO (GPIO_INPUT|GPIO_FLOAT|GPIO_EXTI|GPIO_PORTC|GPIO_PIN7)
 
 __BEGIN_DECLS
 
@@ -21,6 +27,8 @@ public:
 	virtual ssize_t read(struct file *filp, char *buffer, size_t buflen);
 	virtual int ioctl(struct file *filp, int cmd, unsigned long arg);
 	void print_info();
+private:
+	static int irq_handler(int irq, FAR void *context);
 };
 
 HC_SR04::HC_SR04() :
@@ -34,17 +42,34 @@ HC_SR04::~HC_SR04() {
 
 int HC_SR04::init() {
 	CDev::init();
+	stm32_configgpio(GPIO_TRIG);
+	stm32_configgpio(GPIO_ECHO);
+	stm32_gpiosetevent(GPIO_ECHO, false, true, false, irq_handler);
+
 	return 0;
 }
 ssize_t HC_SR04::read(struct file *filp, char *buffer, size_t buflen) {
+	long int a =0;
+	char str1[] = "To be or not to be\n";
+	struct timespec timestart;
+	struct timespec timeend;
+	stm32_gpiowrite(GPIO_TRIG, true);
+	usleep(200);
+	stm32_gpiowrite(GPIO_TRIG, false);
+	usleep(1000);
+	stm32_gpiowrite(GPIO_TRIG, true);
+    a = a;
+	clock_gettime(CLOCK_REALTIME, &timestart);
 
-	char str1[]= "To be or not to be\n";
-	ssize_t strl = strlen (str1);
+	clock_gettime(CLOCK_REALTIME, &timeend);
+
+	a = timeend.tv_nsec - timestart.tv_nsec;
+
+	ssize_t strl = strlen(str1);
 	ssize_t ret = strl > buflen ? buflen : strl;
 	memcpy(buffer, str1, ret);
 	return ret;
 }
-
 
 int HC_SR04::ioctl(struct file *filp, int cmd, unsigned long arg) {
 	int result = OK;
@@ -58,6 +83,10 @@ int HC_SR04::ioctl(struct file *filp, int cmd, unsigned long arg) {
 }
 
 void HC_SR04::print_info() {
+}
+
+int  HC_SR04::irq_handler(int irq, FAR void *context){
+
 }
 
 namespace {
