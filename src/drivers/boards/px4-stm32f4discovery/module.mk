50e0b4e8f0befedb85f7234bcbe5faeee458a26a
Benno Eigenmann <beigenmann@gmail.com>
2014-12-27 12:33:27 +0100

    init

---
+++ src/drivers/boards/px4-stm32f4discovery/module.mk
@@ -0,0 +1,10 @@
+#
+# Board-specific startup code for the PX4FMUv2
+#
+
+SRCS		 = px4discovery_init.c \
+		   px4discovery_usb.c \
+ 		   px4fmu_spi.c\
+		   px4discovery_led.c\
+		   px4fmu_pwm_servo.c\
+		   px4fmu_can.c
